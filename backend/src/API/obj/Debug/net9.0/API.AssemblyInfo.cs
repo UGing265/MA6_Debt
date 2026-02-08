c64db3639ece22b933023de269001bf739389215
F15 <duongquocthai368@gmail.com>
2026-02-08 16:52:32 +0700

    Remove obj and bin directories from cache

--- backend/src/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ backend/src/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8758f30b82a4ffd406c07e01a584d21e03c0ff26")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
